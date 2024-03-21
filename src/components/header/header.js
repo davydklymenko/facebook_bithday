431b72c779249a0a1e0e55e8f4d1a5966b703eca
Davidos <142334488+davydklymenko@users.noreply.github.com>
2024-03-21 12:43:19 +0200

    Cards Press size

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './header.css';
 
+
 class Header extends Component {
     constructor(props) {
         super(props);
@@ -31,16 +32,16 @@ class Header extends Component {
                             </div>
                         )}
 
-                        <li><a href="#">Home</a></li>
+                        <li><a href="home">Home</a></li>
                         <img src="https://upload.wikimedia.org/wikipedia/commons/6/6c/Facebook_Logo_2023.png" alt="logo_facebook"></img>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Celebrate</a></li>
+                        <li><a href='about'>About</a></li>
+                        <li><a href="celebrate">Celebrate</a></li>
                     </div>
 
                     <div className={isMenuOpen ? "dropdown_menu open" : "dropdown_menu"}>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Celebrate</a></li>
+                        <li><a href="home">Home</a></li>
+                        <li><a href='about'>About</a></li>
+                        <li><a href="celebrate">Celebrate</a></li>
                     </div>
                 </header>
             </>
